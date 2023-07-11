d4d24a3f29639736248b154c0519bac26c946d20
Viktor Andreichenko <anvictor0@gmail.com>
2023-07-11 18:49:43 +0200

    added tuocheScreen and wheel events

--- src/controls/controls.js
+++ src/controls/controls.js
@@ -21,15 +21,26 @@ export default function useControls(domElement) {
   const [mouseDown, setMouseDown] = useState(false);
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
   const [keyDown, setkeyDown] = useState(false);
+  const [lastTouch, setLastTouch] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-        setkeyDown(true);
-        return setMoveState((s) => ({ ...s, [keys[event.code]]: true }));
+      setkeyDown(true);
+      return setMoveState((s) => ({ ...s, [keys[event.code]]: true }));
     };
     const handleKeyUp = (event) => {
       setkeyDown(false);
       return setMoveState((s) => ({ ...s, [keys[event.code]]: false }));
+    };
+    const handleWheel = (event) => {
+      setkeyDown(false);
+      setTimeout(() => {
+        handleKeyUp ({code:`${event.deltaY < 0 ? "KeyW" : "KeyS"}`});
+      }, 200);
+      return setMoveState((s) => ({
+        ...s,
+        [keys[`${event.deltaY < 0 ? "KeyW" : "KeyS"}`]]: true,
+      }));
     };
     const handleMouseDown = () => {
       setMouseDown(true);
@@ -45,20 +56,43 @@ export default function useControls(domElement) {
         setMousePos({ x: dx, y: dy });
       }
     };
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
+
+    const handleTouchMove = (event) => {
+      const touch = event.touches[0];
+      const movementX = touch.clientX - lastTouch.x;
+      const movementY = touch.clientY - lastTouch.y;
+      const dx = -movementX * 0.002;
+      const dy = -movementY * 0.002;
+      setMousePos({ x: dx, y: dy });
+      setLastTouch({ x: touch.clientX, y: touch.clientY });
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
     domElement.addEventListener("mousedown", handleMouseDown);
     document.addEventListener("mouseup", handleMouseUp);
     document.addEventListener("mousemove", handleMouseMove);
 
+    domElement.addEventListener("touchstart", handleMouseDown);
+    document.addEventListener("touchend", handleMouseUp);
+    document.addEventListener("touchmove", handleTouchMove);
+
+    document.addEventListener("wheel", handleWheel);
+
     return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
       domElement.removeEventListener("mousedown", handleMouseDown);
       document.removeEventListener("mouseup", handleMouseUp);
       document.removeEventListener("mousemove", handleMouseMove);
+
+      domElement.removeEventListener("touchstart", handleMouseDown);
+      document.removeEventListener("touchend", handleMouseUp);
+      document.removeEventListener("touchmove", handleTouchMove);
+
+      document.removeEventListener("wheel", handleWheel);
     };
-  }, [mouseDown]);
+  }, [mouseDown, lastTouch]);
 
   return [moveState, mouseDown, mousePos, keyDown];
 }
