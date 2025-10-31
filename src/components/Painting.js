30654b8eae47b969f9c79f14dfe77cf8aebcac3f
Victor Andreichenko <anvictor0@gmail.com>
2025-10-31 18:42:12 +0100

    added fly to painting point of view

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -11,11 +11,23 @@ const Painting = ({ data, getFlyData }) => {
     "/vr_gallery/paintings/SquareShadowBlack.png"
   );
   const brightness = 80;
+  const distanceAbsolut = height * 0.0632 + 5.5;
+  const sign = rotation[1] === 0 ? 1 : rotation[1] === 1.5708 ? 1 : -1;
   const pos3d = new THREE.Vector3(1, 0, 0);
   const handleClick = () => {
-    pos3d.x = position[0];
+    pos3d.x =
+      rotation[1] === 1.5708
+        ? position[0] + sign * distanceAbsolut
+        : rotation[1] === -1.5708
+        ? position[0] + sign * distanceAbsolut
+        : position[0];
     pos3d.y = position[1];
-    pos3d.z = position[2];
+    pos3d.z =
+      rotation[1] === 0
+        ? position[2] + sign * distanceAbsolut
+        : rotation[1] === 3.14159
+        ? position[2] + sign * distanceAbsolut
+        : position[2];
     getFlyData(pos3d);
   };
 
