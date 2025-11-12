8277077673478711a1659510a58442159e562b39
Victor Andreichenko <anvictor0@gmail.com>
2025-11-12 16:07:14 +0100

    monitorTV frame and shadow

--- src/components/MonitorTV.js
+++ src/components/MonitorTV.js
@@ -1,6 +1,13 @@
 // MonitorTV.js
 import React, { useEffect, useRef } from "react";
-import { VideoTexture, MeshBasicMaterial, DoubleSide, Vector2 } from "three";
+import {
+  VideoTexture,
+  MeshBasicMaterial,
+  DoubleSide,
+  TextureLoader,
+  Vector2,
+} from "three";
+import { useLoader } from "@react-three/fiber";
 import * as THREE from "three";
 import Label from "./Label";
 
@@ -10,9 +17,9 @@ const MonitorTV = ({ flyToPoint }) => {
   if (!videoRef.current) {
     const video = document.createElement("video");
     video.src = "/vr_gallery/paintings/SculptureHead.mp4";
-    video.muted = true; // без звуку
-    video.loop = true; // нескінченний цикл
-    video.playsInline = true; // для мобільних
+    video.muted = true;
+    video.loop = true;
+    video.playsInline = true;
     video.setAttribute("playsinline", "");
     videoRef.current = video;
   }
@@ -20,7 +27,7 @@ const MonitorTV = ({ flyToPoint }) => {
   const videoTexture = new VideoTexture(videoRef.current);
   videoTexture.offset = new Vector2(0.1, 0.1);
   videoTexture.repeat = new Vector2(0.7, 0.7);
-  const material = new MeshBasicMaterial({
+  const monitorMaterial = new MeshBasicMaterial({
     map: videoTexture,
     side: DoubleSide,
   });
@@ -32,7 +39,7 @@ const MonitorTV = ({ flyToPoint }) => {
     });
     return () => {
       videoTexture.dispose();
-      material.dispose();
+      monitorMaterial.dispose();
     };
     // eslint-disable-next-line
   }, []);
@@ -47,20 +54,114 @@ const MonitorTV = ({ flyToPoint }) => {
     flyToPoint(pos3d);
   };
 
+  // ✅ Розміри монітора та рамки
+  const monitorWidth = 45;
+  const monitorHeight = 70;
+  const frameThickness = 3.33;
+  const halfT = frameThickness / 2;
+
+  // ✅ Матеріали для граней рамки
+  const materialFront = new MeshBasicMaterial({ color: "#444" });
+  const materialSide = new MeshBasicMaterial({ color: "#333" });
+  const materialTopBottom = new MeshBasicMaterial({ color: "#222" });
+
+  // ✅ Тінь під монітором
+  const shadowTexture = useLoader(
+    TextureLoader,
+    "/vr_gallery/paintings/SquareShadowBlack.png"
+  );
+
   return (
     <group
       position={[-115, 180, 50]}
       rotation={[0, 1.5708, 0]}
       onPointerUp={handleFlyToMonitor}
     >
-      <mesh material={material} position={[0, 0, 2]}>
-        <planeGeometry args={[45, 70]} />
+      {/* Полотно монітора */}
+      <mesh material={monitorMaterial} position={[0, 0, 2]}>
+        <planeGeometry args={[monitorWidth, monitorHeight]} />
       </mesh>
+
+      {/* ✅ Тінь (більша, темніша, розмита) */}
+      <mesh position={[0, -3.5, 0.8]}>
+        <planeGeometry args={[monitorWidth * 1.3, monitorHeight * 1.3]} />
+        <meshBasicMaterial map={shadowTexture} transparent opacity={0.6} />
+      </mesh>
+
+      {/* ✅ Рамка-багет з гранями */}
+      {/* Верхній брусок */}
+      <mesh position={[0, monitorHeight / 2 + halfT, 2 + halfT]}>
+        <boxGeometry
+          args={[
+            monitorWidth + frameThickness * 2,
+            frameThickness,
+            frameThickness,
+          ]}
+        />
+        <meshBasicMaterial attach="material-0" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-1" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-2" {...materialFront} />
+        <meshBasicMaterial attach="material-3" {...materialFront} />
+        <meshBasicMaterial attach="material-4" {...materialSide} />
+        <meshBasicMaterial attach="material-5" {...materialSide} />
+      </mesh>
+
+      {/* Нижній брусок */}
+      <mesh position={[0, -monitorHeight / 2 - halfT, 2 + halfT]}>
+        <boxGeometry
+          args={[
+            monitorWidth + frameThickness * 2,
+            frameThickness,
+            frameThickness,
+          ]}
+        />
+        <meshBasicMaterial attach="material-0" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-1" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-2" {...materialFront} />
+        <meshBasicMaterial attach="material-3" {...materialFront} />
+        <meshBasicMaterial attach="material-4" {...materialSide} />
+        <meshBasicMaterial attach="material-5" {...materialSide} />
+      </mesh>
+
+      {/* Лівий брусок */}
+      <mesh position={[-monitorWidth / 2 - halfT, 0, 2 + halfT]}>
+        <boxGeometry
+          args={[
+            frameThickness,
+            monitorHeight + frameThickness * 2,
+            frameThickness,
+          ]}
+        />
+        <meshBasicMaterial attach="material-0" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-1" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-2" {...materialSide} />
+        <meshBasicMaterial attach="material-3" {...materialSide} />
+        <meshBasicMaterial attach="material-4" {...materialFront} />
+        <meshBasicMaterial attach="material-5" {...materialFront} />
+      </mesh>
+
+      {/* Правий брусок */}
+      <mesh position={[monitorWidth / 2 + halfT, 0, 2 + halfT]}>
+        <boxGeometry
+          args={[
+            frameThickness,
+            monitorHeight + frameThickness * 2,
+            frameThickness,
+          ]}
+        />
+        <meshBasicMaterial attach="material-0" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-1" {...materialTopBottom} />
+        <meshBasicMaterial attach="material-2" {...materialSide} />
+        <meshBasicMaterial attach="material-3" {...materialSide} />
+        <meshBasicMaterial attach="material-4" {...materialFront} />
+        <meshBasicMaterial attach="material-5" {...materialFront} />
+      </mesh>
+
       <Label
         author={"Viktor Andreichenko"}
         name={"Sculpture Head"}
-        width={450}
-        height={700}
+        width={monitorWidth * 10}
+        height={monitorHeight * 10}
       />
     </group>
   );
